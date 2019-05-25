347e20f9df9eecdd05de64666666538369069403
OneAutumnLeaf95 <51017206+OneAutumnLeaf95@users.noreply.github.com>
2019-05-25 15:46:00 +0700

    Java Project
    Mesin Kasir Sederhana

---
+++ TugasAkhir/src/tugasakhir/ClassMKasir.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasakhir;
+
+/**
+ *
+ * @author One Autumn Leaf
+ */
+public class ClassMKasir {
+    
+    private String hasil;
+    
+    public void sethasil(String hasil){
+        this.hasil = hasil;
+    }
+        
+    public String gethasil(){
+        return hasil;
+    }
+}
